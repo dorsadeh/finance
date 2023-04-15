3f75367f2783db4453d4f7b20dd44c8bc39ce82c
Dor Sadeh <dorobm@gmail.com>
2023-04-15 16:04:41 +0300

    some processing added for filtering dividend yield and payout ratio

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os.path
+
 import yfinance as yf
 import pandas as pd
 
@@ -8,31 +10,61 @@ tickers = ['JNJ', 'XOM', 'CVX', 'KO', 'MCD', 'RTX', 'IBM', 'ADP', 'TGT', 'ITW', 
 metrics = ['dividendYield', 'payoutRatio', 'trailingPE', 'forwardPE', 'enterpriseToEbitda', 'totalDebt',
            'totalCash']
 
-# Create an empty list to store the data for each ticker
-ticker_data = []
+output_file_name = 'ticker_data.csv'
+force_update_csv_file = False
 
-# Loop through each ticker and retrieve the data for the specified metrics
-for ticker in tickers:
-    # Download the ticker data using yfinance
-    print("===========" + ticker + "===========")
-    print("downloading data...")
-    ticker_info = yf.Ticker(ticker)
+def get_data():
+    # Create an empty list to store the data for each ticker
+    ticker_data = []
 
-    print("processing data...")
-    # Extract the data for the specified metrics and store it in a dictionary
-    data = {}
-    for metric in metrics:
-        try:
-            value = ticker_info.info[metric]
-        except:
-            value = 'N/A'
-        data[metric] = value
+    # Loop through each ticker and retrieve the data for the specified metrics
+    for ticker in tickers:
+        # Download the ticker data using yfinance
+        print("===========" + ticker + "===========")
+        print("downloading data...")
+        ticker_info = yf.Ticker(ticker)
 
-    # Add the ticker's data to the list of data for all tickers
-    ticker_data.append(data)
+        print("processing data...")
+        # Extract the data for the specified metrics and store it in a dictionary
+        data = {}
+        for metric in metrics:
+            try:
+                value = ticker_info.info[metric]
+            except:
+                value = 'N/A'
+            data[metric] = value
 
-# Convert the list of dictionaries to a Pandas DataFrame
-df = pd.DataFrame(ticker_data, index=tickers)
+        # Add the ticker's data to the list of data for all tickers
+        ticker_data.append(data)
 
-# Save the data to a CSV file
-df.to_csv('ticker_data.csv')
+    # Convert the list of dictionaries to a Pandas DataFrame
+    df = pd.DataFrame(ticker_data, index=tickers)
+
+    # Save the data to a CSV file
+    df.to_csv(output_file_name)
+
+def process_data():
+    DIVIDEND_YIELD_MIN_VAL = 0.025
+    PAYOUT_RATIO_MAX_VAL = 0.7
+
+    print("======= all data ======")
+    df1 = pd.read_csv(output_file_name)
+    print(df1.to_string())
+
+    print("\n======= filter dividend yield ======")
+    df2 = df1[df1['dividendYield'] > DIVIDEND_YIELD_MIN_VAL]
+    print(df2.to_string())
+
+    print("\n======= filter payout ratio ======")
+    df3 = df2[df2['payoutRatio'] < PAYOUT_RATIO_MAX_VAL]
+    print(df3.to_string())
+
+
+if __name__ == '__main__':
+    csv_file_exists = os.path.exists(output_file_name)
+    if csv_file_exists and not force_update_csv_file:
+        print("data exists. not updating file...")
+    else:
+        print("updating data for all tickers")
+        get_data()
+    process_data()
