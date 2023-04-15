3b2ecb7e8e26b1f51c08f03e07c9fddbbf6aed14
Dor Sadeh <dorobm@gmail.com>
2023-04-15 11:49:47 +0300

    data gathered for a list of tickers

--- main.py
+++ main.py
@@ -1,6 +1,65 @@
 import yfinance as yf
-import json
+import pandas as pd
 
-ticker = yf.Ticker("AAPL")
-json_formatted_str = json.dumps(ticker.info, indent=2)
-print(json_formatted_str)
+# tickers = ['JNJ', 'XOM', 'CVX', 'KO']
+tickers = ['JNJ', 'XOM', 'CVX', 'KO', 'MCD', 'RTX', 'IBM', 'ADP', 'TGT', 'ITW', 'CL', 'APD', 'EMR', 'AFL', 'ED', 'WBA', 'GPC', 'CLX', 'FC', 'PII', 'SON', 'LEG', 'MGEE', 'WLYB', 'UVV', 'TDS', 'ARTNA', 'MMM']
+
+# Define a list of the metrics we want to retrieve
+metrics = ['dividendYield', 'payoutRatio', 'trailingPE', 'forwardPE', 'enterpriseToEbitda', 'totalDebt',
+           'totalCash']
+
+# Create an empty list to store the data for each ticker
+ticker_data = []
+
+# Loop through each ticker and retrieve the data for the specified metrics
+for ticker in tickers:
+    # Download the ticker data using yfinance
+    print("===========" + ticker + "===========")
+    print("downloading data...")
+    ticker_info = yf.Ticker(ticker)
+
+    print("processing data...")
+    # Extract the data for the specified metrics and store it in a dictionary
+    data = {}
+    for metric in metrics:
+        try:
+            value = ticker_info.info[metric]
+        except:
+            value = 'N/A'
+        data[metric] = value
+
+    # Add the ticker's data to the list of data for all tickers
+    ticker_data.append(data)
+
+# Convert the list of dictionaries to a Pandas DataFrame
+df = pd.DataFrame(ticker_data, index=tickers)
+
+# Save the data to a CSV file
+df.to_csv('ticker_data.csv')
+
+# import yfinance as yf
+# import json
+#
+#
+# # def get_stocks_info(tickers_list: list, stocks_info_list: list):
+# #     for ticker in tickers_list:
+# #         print("getting info for " + ticker)
+# #         ticker_json = yf.Ticker(ticker)
+# #         json_formatted_str = json.dumps(ticker_json.info, indent=2)
+# #         print(json_formatted_str)
+# #         stocks_info_list.append(json_formatted_str)
+#
+#
+#
+# if __name__ == '__main__':
+#     # tickers = ['JNJ', 'XOM', 'CVX', 'KO']
+#     tickers_list = ['JNJ', 'XOM']
+#     all_tickers = ' '.join(tickers_list)
+#     tickers = yf.Tickers(all_tickers)
+#
+#     for ticker_str in tickers_list:
+#         stockInfo = StockInfo()
+#         stockInfo.dividend_yield = tickers_list[ticker_str].info["dividendYield"]
+
+
+
