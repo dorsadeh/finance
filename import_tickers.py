c63b87820f0f24e432d58af0aea5a9069c034995
Dor Sadeh <dorobm@gmail.com>
2023-04-29 17:40:10 +0300

    create get_tickers_list to prevent code duplication

--- import_tickers.py
+++ import_tickers.py
@@ -2,48 +2,42 @@
 
 import pandas as pd
 import requests
+import json
 
 ticker_dict = {}
-# Download the list of S&P 500 companies from Wikipedia
-url = 'https://en.wikipedia.org/wiki/S%26P_500_Dividend_Aristocrats'
-html = requests.get(url).content
-df_list = pd.read_html(html)
-df = df_list[-1]
-dividend_aristocrats_list = df["Ticker symbol"].to_list()
-ticker_dict["dividend_aristocrats"] = dividend_aristocrats_list
+output_file_name = "tickers_lists.json"
+source = ["wikipedia", "digrin"]
 
-# Download the list of Chalengers from digin
-url = 'https://www.digrin.com/stocks/list/7-challengers/'
-html = requests.get(url).content
-df_list = pd.read_html(html)
-df = df_list[-1]
-ticker_and_name_list = df["Stock"].to_list()
-challengers = []
-for item in ticker_and_name_list:
-    challengers.append(item.split()[0])
-ticker_dict["challengers"] = challengers
+def get_tickers_list(url:str, source: str) -> dict:
+    """
+    downloads data from the url and extracts a list of tickers to a list
+    """
+    # Download the list of S&P 500 companies from Wikipedia
+    print("getting data from " + url)
+    html = requests.get(url).content
+    df_list = pd.read_html(html)
+    df = df_list[-1]
+    if source == "wikipedia":
+        return df["Ticker symbol"].to_list()
+    elif source == "digrin":
+        ticker_list = []
+        for item in df["Stock"].to_list():
+            ticker_list.append(item.split()[0])
+        return ticker_list
+    else:
+        print("unspecified source!!!")
+        return {}
+    
 
-url = 'https://www.digrin.com/stocks/list/6-contenders/'
-html = requests.get(url).content
-df_list = pd.read_html(html)
-df = df_list[-1]
-ticker_and_name_list = df["Stock"].to_list()
-contenders = []
-for item in ticker_and_name_list:
-    contenders.append(item.split()[0])
-ticker_dict["contenders"] = contenders
+ticker_dict["dividend_aristocrats"] = get_tickers_list('https://en.wikipedia.org/wiki/S%26P_500_Dividend_Aristocrats', "wikipedia")
+ticker_dict["challengers"] = get_tickers_list('https://www.digrin.com/stocks/list/7-challengers/', "digrin")
+ticker_dict["contenders"] = get_tickers_list('https://www.digrin.com/stocks/list/6-contenders/', "digrin")
+ticker_dict["champions"] = get_tickers_list('https://www.digrin.com/stocks/list/5-champions/', "digrin")
 
-url = 'https://www.digrin.com/stocks/list/5-champions/'
-html = requests.get(url).content
-df_list = pd.read_html(html)
-df = df_list[-1]
-ticker_and_name_list = df["Stock"].to_list()
-champions = []
-for item in ticker_and_name_list:
-    champions.append(item.split()[0])
-ticker_dict["champions"] = champions
+print("==================================================")
+for item in ticker_dict:
+    print(item + " list has " + str(len(ticker_dict[item])) + " tickers")
 
-
-import json
-with open("tickers_lists.json", 'w') as f:
+print("saving data to " + output_file_name)
+with open(output_file_name, 'w') as f:
     json.dump(ticker_dict, f, indent=4)
