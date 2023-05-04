f6a3a208cdec8b2b55558f8cf1c6f3ea60fd0be3
talobs <talobs5492@gmail.com>
2023-05-04 14:23:20 +0300

    changes exp_growth to DGR (Dividend Growth Rate)

--- analyzer.py
+++ analyzer.py
@@ -6,13 +6,9 @@ import scipy as cp
 DAYS_PER_YEAR = 365
 
 class Analyzer():
-    """
-    This function calculate the average exponential increments of the dividend
-    and check wheter it is monotonically increasing and if it persistent with
-    maximal time between dividends of 100 days
-    """
-    def __init__(self, dividends_df: pd.core.series.Series, exp_years: float) -> None:
-        self.exp_years = exp_years
+    #TODO: Doc this class
+    def __init__(self, dividends_df: pd.core.series.Series, DGR_years: float) -> None:
+        self.DGR_years = DGR_years
         self.number_of_events = dividends_df.size
         self.t_abs = dividends_df["Date"].values
         t_ns = np.datetime64('today') - self.t_abs
@@ -20,7 +16,7 @@ class Analyzer():
         self.t_years = -self.t_days.astype('float64')/DAYS_PER_YEAR
         self.divs_values = dividends_df["Dividends"].values
         
-        self._exp_mean_yearly_growth = math.nan
+        self._DGR = math.nan
         self._mean_time_between_events = math.nan
         self._growth_streak = math.nan
         self._always_monotonic = False
@@ -28,13 +24,13 @@ class Analyzer():
         self._always_persistent = False
         
         if self.number_of_events > 2:
-            self.exp_growth()
+            self.cal_DGR()
             self.cal_monotonicity()
             self.cal_persistency()
     
     def get_compact_data(self) -> dict:
         return {
-            "exp_mean_yearly_growth": self._exp_mean_yearly_growth,
+            "DGR": self._DGR,
             "mean_time_between_dividends": self._mean_time_between_events,
             "growth_streak": self._growth_streak,
             "always_monotonic": self._always_monotonic,
@@ -42,8 +38,8 @@ class Analyzer():
         }
 
     @property
-    def exp_mean_yearly_growth(self) -> float:
-        return self._exp_mean_yearly_growth
+    def DGR(self) -> float:
+        return self._DGR
     
     @property
     def mean_time_between_dividends(self) -> float:
@@ -66,16 +62,16 @@ class Analyzer():
         return self._always_persistent
 
 
-    def exp_growth(self):
+    def cal_DGR(self):
         """
-        This function fits the data from the last number_of_years to an exponential model and calculates the exp_mean_yearly_growth
+        This function fits the data from the last number_of_years to an exponential model and calculates dividend growth rate (DGR)
         """
-        last_events_indices = self.t_years > -self.exp_years
+        last_events_indices = self.t_years > -self.DGR_years
         t_years_last = self.t_years[last_events_indices]
         divs_values_last = self.divs_values[last_events_indices]
         divs_values_log = np.log(divs_values_last)
         linreg_result = cp.stats.linregress(t_years_last, divs_values_log)
-        self._exp_mean_yearly_growth = np.exp(linreg_result.slope)-1
+        self._DGR = (np.exp(linreg_result.slope)-1)
 
     def cal_monotonicity(self):
         # checking if dividends are monotonically increasing
